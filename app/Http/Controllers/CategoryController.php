894ec28aed14cce30912048e30e6eca21a37fc3f
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-19 16:24:32 -0500

    return view categories index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RequestCategory;
 use App\Models\Category;
+
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
 
 class CategoryController extends Controller
@@ -14,12 +16,14 @@ class CategoryController extends Controller
 
     public function index()
     {
-        //
+        $categories = Category::all();
+
+        return view('views.categories.index', compact("categories"));
     }
 
     public function createCategory()
     {
-        //
+        return view('views.categories.create');
     }
 
     public function saveCategory(RequestCategory $request): RedirectResponse
