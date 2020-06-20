32de02dc34c169992c93a8cdabacd9fd5a0a9e11
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-20 15:48:03 -0500

    name and description required udpate

--- tests/Feature/UpdateCategoryTest.php
+++ tests/Feature/UpdateCategoryTest.php
@@ -26,6 +26,24 @@ class UpdateCategoryTest extends TestCase
      * @return void
      * @test
      */
+    public function a_category_required_name_and_description(): void
+    {
+        $category = factory(Category::class)->create();
+
+        $response = $this->from(route("categories.edit", $category->id))->put(route("categories.update", $category->id), [
+            "name"          => "",
+            "description"   => ""
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.edit", $category->id));
+        $response->assertSessionHasErrors(["name", "description"]);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
     public function a_user_can_update_category(): void
     {
         $category = factory(Category::class)->create();
