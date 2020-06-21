5639a15a01a175b685847a169c7302d8647460e6
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-21 09:03:52 -0500

    description required min 10 characters

--- tests/Feature/UpdateCategoryTest.php
+++ tests/Feature/UpdateCategoryTest.php
@@ -66,6 +66,23 @@ class UpdateCategoryTest extends TestCase
      * @return void
      * @test
      */
+    public function a_category_required_min_10_characters_in_description(): void
+    {
+        $category = factory(Category::class)->create();
+
+        $response = $this->from(route("categories.edit",$category->id))->put(route("categories.update", $category->id), [
+            "description" => "Lorem"
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.edit", $category->id));
+        $response->assertSessionHasErrors("description");
+    }
+
+    /**
+     * @return void
+     * @test
+     */
     public function a_user_can_update_category(): void
     {
         $category = factory(Category::class)->create();
