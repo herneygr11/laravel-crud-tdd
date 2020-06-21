c6b210e4f1354d30a99781149b3b01c0f6c1aa62
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-21 09:18:04 -0500

    update category test

---
+++ tests/Browser/UsersCanupdateCategoriesTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+use function PHPSTORM_META\type;
+
+class UsersCanupdateCategoriesTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @return void
+     * @test
+     */
+    public function user_can_update_categories(): void
+    {
+        $category = factory(Category::class)->create();
+
+        $this->browse(function (Browser $browser) use ($category) {
+            $browser->visit(route("categories.edit", $category->id))
+                ->assertSee('Editar Categoria')
+                ->clear("name")
+                ->clear("description")
+                ->type("name", "Fromted")
+                ->type("description", "Desarrollador del lado del cliente")
+                ->press("#update-category")
+                ->assertSee("Fromted")
+                ->assertSee("Desarrollador del lado del cliente");
+        });
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function view_button_cancelar(): void
+    {
+        $category = factory(Category::class)->create();
+
+        $this->browse(function (Browser $browser) use ($category){
+            $browser->visit(route("categories.edit", $category->id))
+                ->assertSeeLink("Cancelar");
+        });
+    }
+}
