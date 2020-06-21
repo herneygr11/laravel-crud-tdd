f337401164ecdf085d24296bf97b9c11eb251303
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-21 09:19:23 -0500

    view edit category

--- resources/views/views/categories/edit.blade.php
+++ resources/views/views/categories/edit.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.layout')
 
 @section('main')
-    <h1>Category</h1>
+    <h1>Editar Categoria</h1>
+    <form action="{{ route("categories.update", $category->id) }}" method="post">
+        @csrf
+        @method("PUT")
+
+        <input type="text" name="name" value="{{$category->name}}">
+        <br>
+        <textarea name="description" cols="30" rows="10">{{$category->description}}</textarea>
+
+        <a href="{{ route("categories.index") }}">Cancelar</a>
+        <button id="update-category">Guardar</button>
+    </form>
 @endsection
