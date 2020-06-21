4da5eee8c4a891c0b1e24f7ab1ced5ddd478f729
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-21 09:06:08 -0500

    description required max 200 characters

--- tests/Feature/UpdateCategoryTest.php
+++ tests/Feature/UpdateCategoryTest.php
@@ -66,11 +66,28 @@ class UpdateCategoryTest extends TestCase
      * @return void
      * @test
      */
+    public function a_category_required_max_200_characters_in_description(): void
+    {
+        $category = factory(Category::class)->create();
+
+        $response = $this->from(route("categories.edit", $category->id))->put(route("categories.update", $category->id), [
+            "description" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("categories.edit", $category->id));
+        $response->assertSessionHasErrors("description");
+    }
+
+    /**
+     * @return void
+     * @test
+     */
     public function a_category_required_min_10_characters_in_description(): void
     {
         $category = factory(Category::class)->create();
 
-        $response = $this->from(route("categories.edit",$category->id))->put(route("categories.update", $category->id), [
+        $response = $this->from(route("categories.edit", $category->id))->put(route("categories.update", $category->id), [
             "description" => "Lorem"
         ]);
 
