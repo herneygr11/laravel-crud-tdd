c51c6ed67953cd466eeedad1c7a5baf31c14470d
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-25 09:17:47 -0500

    view index

--- resources/views/views/roles/index.blade.php
+++ resources/views/views/roles/index.blade.php
@@ -1,1 +1,25 @@
 @extends("layouts.layout")
+
+@section('main')
+    <h2>Roles</h2>
+    <a href="{{ route("roles.create") }}">Crear Rol</a>
+
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>name</th>
+                <th>descripcion</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($roles as $role)
+                <tr>
+                <td>{{ $role->id }}</td>
+                <td>{{ $role->name }}</td>
+                <td>{{ $role->description }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
