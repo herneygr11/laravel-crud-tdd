e756af8a692cd8e4693b2520c02aad5acf951c0b
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-26 09:01:12 -0500

    updarte role test

--- tests/Feature/UpdateRoleTest.php
+++ tests/Feature/UpdateRoleTest.php
@@ -27,10 +27,31 @@ class UpdateRoleTest extends TestCase
      */
     public function a_user_can_see_the_update_view(): void
     {
-        $this->withoutExceptionHandling();
         $role = factory(Role::class)->create();
 
         $response = $this->get(route("roles.edit", $role->id));
         $response->assertStatus(200);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function a_user_can_update_roles(): void
+    {
+        $role = factory(Role::class)->create();
+
+        $rolePut = [
+            "name"          => "Desarrollador Backend",
+            "description"   => "Desarrolaldor del lado del servidor",
+            "guard_name"    => "web",
+        ];
+
+        $response = $this->from(route("roles.edit", $role->id))->put(route("roles.update", $role->id), $rolePut);
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route("roles.index"));
+
+        $this->assertDatabaseHas("roles", $rolePut);
     }
 }
