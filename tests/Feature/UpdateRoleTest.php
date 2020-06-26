20e3a2b4a9992e68486a79d96ec89fffbc6e9968
Herney Ruiz <herneyruiz36@gmail.com>
2020-06-26 08:16:16 -0500

    user can see view update role test

--- tests/Feature/UpdateRoleTest.php
+++ tests/Feature/UpdateRoleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,10 +17,20 @@ class UpdateRoleTest extends TestCase
      */
     public function a_user_can_see_the_index_roles_view(): void
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->get(route("roles.index"));
         $response->assertStatus(200);
     }
 
+    /**
+     * @return void
+     * @test
+     */
+    public function a_user_can_see_the_update_view(): void
+    {
+        $this->withoutExceptionHandling();
+        $role = factory(Role::class)->create();
+
+        $response = $this->get(route("roles.edit", $role->id));
+        $response->assertStatus(200);
+    }
 }
