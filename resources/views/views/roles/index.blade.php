00dc78d07070426313ff579c6686deb28df4f5ee
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-10 07:21:13 -0500

    user can view roles

--- resources/views/views/roles/index.blade.php
+++ resources/views/views/roles/index.blade.php
@@ -1,25 +1,34 @@
 @extends("layouts.layout")
 
 @section('main')
-    <h2>Roles</h2>
-    <a href="{{ route("roles.create") }}">Crear Rol</a>
+<h2>Roles</h2>
+<a href="{{ route("roles.create") }}">Crear Rol</a>
 
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>name</th>
-                <th>descripcion</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($roles as $role)
-                <tr>
-                <td>{{ $role->id }}</td>
-                <td>{{ $role->name }}</td>
-                <td>{{ $role->description }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+            <th>descripcion</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($roles as $role)
+        <tr>
+            <td>{{ $role->id }}</td>
+            <td>{{ $role->name }}</td>
+            <td>{{ $role->description }}</td>
+            <td>
+                <a href="{{ route('roles.edit', $role->id) }}">
+                    Editar
+                </a>
+                <form action="{{ route('roles.delete', $role->id) }}" method="post">
+                    <button type="submit">Eliminar</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
