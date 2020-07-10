140d9aef0a6a734fb4dd51b5c3a2fcff9bda091d
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-10 07:20:51 -0500

    user can view roles test

---
+++ tests/Browser/UserCanViewRolesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Role;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class UserCanViewRolesTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @return void
+     * @test
+     */
+    public function user_can_view_roles()
+    {
+        $role = factory(Role::class)->create();
+
+        $this->browse(function (Browser $browser) use ($role) {
+            $browser->visit(route("roles.index"))
+                ->assertSee($role->name)
+                ->assertSee($role->description)
+                ->assertSee('Roles')
+                ->assertSeeLink('Crear Rol')
+                ->assertSeeLink('Editar')
+                ->assertSee('Eliminar')
+            ;
+        });
+    }
+}
