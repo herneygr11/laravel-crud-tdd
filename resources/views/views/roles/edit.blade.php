ff7a43e3d0a7ed1fda0295a2f9f3e3a7e22c8238
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-12 07:02:20 -0500

    view update roles

--- resources/views/views/roles/edit.blade.php
+++ resources/views/views/roles/edit.blade.php
@@ -1,1 +1,27 @@
 @extends("layouts.layout")
+
+@section('main')
+<h1>Editar Rol</h1>
+<form action="{{ route("roles.update", 1) }}" method="post">
+    @csrf
+    @method("PUT")
+
+    <input type="text" name="name" value="{{$role->name}}">
+    @if ($errors->has("name"))
+        <span>{{ $errors->first("name") }}</span>
+    @endif
+    <br>
+    <select name="guard_name">
+        <option value="web" @if ("web" === $role->guard_name) selected @endif >Web</option>
+        <option value="api" @if ("api" === $role->guard_name) selected @endif>Api</option>
+    </select>
+    <br>
+    <textarea name="description" cols="30" rows="10">{{$role->description}}</textarea>
+    @if ($errors->has("description"))
+        <span>{{ $errors->first("description") }}</span>
+    @endif
+
+    <a href="{{ route("categories.index") }}">Cancelar</a>
+    <button id="update-role">Guardar</button>
+</form>
+@endsection
