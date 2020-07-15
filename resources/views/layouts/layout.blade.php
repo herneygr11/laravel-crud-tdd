09a42ce160886edae462184574633f9fdf8f0d0e
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-15 07:23:01 -0500

    link css y js, and styles the navbar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,35 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{env('APP_NAME')}}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <header class="header">
-        <span class="header__title">
-            LCT
-        </span>
-        <nav class="nav">
-            <ul class="nav__content-item">
-                <li class="nav__item">
-                    <a href="#">Categorias</a>
-                </li>
-                <li class="nav__item">
-                    <a href="#">Estados</a>
-                </li>
-                <li class="nav__item">
-                    <a href="#">Grupos</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <div class="container">
 
-    <main class="main">
-        @yield('main')
-    </main>
+        <header class="header navbar navbar-expand-lg navbar-light bg-light">
+            <span class="header__title">
+                LCT
+            </span>
+            <nav class="nav collapse navbar-collapse">
+                <ul class="nav__content-item navbar-nav ml-auto">
+                    <li class="nav__item nav-item">
+                        <a href="#" class="nav-link">Categorias</a>
+                    </li>
+                    <li class="nav__item nav-item">
+                        <a href="#" class="nav-link">Estados</a>
+                    </li>
+                    <li class="nav__item nav-item">
+                        <a href="#" class="nav-link">Grupos</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
 
-    <footer class="footer">
-        <span>Develop by <a href="#">Herney Ruiz</a></span>
-    </footer>
+        <main class="main">
+            @yield('main')
+        </main>
+
+        <footer class="footer">
+            <span>Develop by <a href="#">Herney Ruiz</a></span>
+        </footer>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
