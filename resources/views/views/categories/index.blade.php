9a7203ee587dfd533d961fc41c20fa1f86829a87
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-16 07:31:03 -0500

    styles in the table

--- resources/views/views/categories/index.blade.php
+++ resources/views/views/categories/index.blade.php
@@ -2,15 +2,18 @@
 
 @section('main')
 
-<a href="{{ route("categories.create") }}">Crear categoria</a>
-<h3>Categorias</h3>
+<div class="d-flex align-items-center justify-content-between my-5">
+    <h3>Categorias</h3>
+    <a href="{{ route("categories.create") }}" class="btn btn-primary">Crear categoria</a>
+</div>
 
-<table>
+<table class="table table-active table-hover w-50 m-auto">
     <thead>
         <tr>
             <th>#</th>
             <th>name</th>
             <th>Description</th>
+            <th>Acciones</th>
         </tr>
     </thead>
     <tbody>
@@ -19,12 +22,12 @@
             <td>{{ $category->id }}</td>
             <td>{{ $category->name }}</td>
             <td>{{ $category->description }}</td>
-            <td>
-                <a href="{{ route("categories.edit", $category->id) }}">editar</a>
+            <td class="d-flex align-content-center">
+                <a href="{{ route("categories.edit", $category->id) }}" class="btn btn-primary mx-1">editar</a>
                 <form action="{{ route('categories.delete', $category->id) }}" method="POST">
                     @csrf
                     @method("DELETE")
-                    <button id="delete-category">eliminar</button>
+                    <button id="delete-category" class="btn btn-danger mx-1">eliminar</button>
                 </form>
             </td>
         </tr>
