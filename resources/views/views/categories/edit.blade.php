be63d71425d153ffcc7761080eb682363ce8a73c
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-17 07:47:40 -0500

    view edit apply margin top

--- resources/views/views/categories/edit.blade.php
+++ resources/views/views/categories/edit.blade.php
@@ -1,36 +1,39 @@
 @extends('layouts.layout')
 
 @section('main')
-<div class="card w-50 m-auto">
-    <div class="card-header">
-        <h1>Editar Categoria</h1>
-    </div>
-    <div class="card-body">
-        <form action="{{ route("categories.update", $category->id) }}" method="post">
-            @csrf
-            @method("PUT")
+<div class="mt-5">
+    <div class="card w-50 m-auto">
+        <div class="card-header">
+            <h1>Editar Categoria</h1>
+        </div>
+        <div class="card-body">
+            <form action="{{ route("categories.update", $category->id) }}" method="post">
+                @csrf
+                @method("PUT")
 
-            <div class="form-group my-2">
-                <label for="name">Nombre</label>
-                <input type="text" name="name" class="form-control" value="{{$category->name}}">
-                @if ($errors->has("name"))
-                <span class="text-danger">{{ $errors->first("name") }}</span>
-            </div>
-            @endif
+                <div class="form-group my-2">
+                    <label for="name">Nombre</label>
+                    <input type="text" name="name" class="form-control" value="{{$category->name}}">
+                    @if ($errors->has("name"))
+                    <span class="text-danger">{{ $errors->first("name") }}</span>
+                    @endif
+                </div>
 
-            <div class="form-group my-2">
-                <label for="description">Descripcion</label>
-                <textarea name="description" cols="30" rows="10" class="form-control">{{$category->description}}</textarea>
-                @if ($errors->has("description"))
-                <span class="text-danger">{{ $errors->first("description") }}</span>
-                @endif
-            </div>
+                <div class="form-group my-2">
+                    <label for="description">Descripcion</label>
+                    <textarea name="description" cols="30" rows="5"
+                        class="form-control">{{$category->description}}</textarea>
+                    @if ($errors->has("description"))
+                    <span class="text-danger">{{ $errors->first("description") }}</span>
+                    @endif
+                </div>
 
-            <div class="d-flex align-items-center justify-content-end">
-                <a href="{{ route("categories.index") }}" class="btn btn-warning text-white mx-1">Cancelar</a>
-                <button id="update-category" class="btn btn-primary mx-1">Guardar</button>
-            </div>
-        </form>
+                <div class="d-flex align-items-center justify-content-end">
+                    <a href="{{ route("categories.index") }}" class="btn btn-warning text-white mx-1">Cancelar</a>
+                    <button id="update-category" class="btn btn-primary mx-1">Guardar</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
