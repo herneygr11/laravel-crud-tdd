f7c298d0bab213568fe94cb1c2aff4a672f05b03
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-17 07:47:58 -0500

    styles view create categories

--- resources/views/views/categories/create.blade.php
+++ resources/views/views/categories/create.blade.php
@@ -1,11 +1,38 @@
 @extends('layouts.layout')
 
 @section('main')
-<form action="{{ route("categories.save") }}" method="post">
-    <h3>Crear Categoria</h3>
-    <input type="text" name="name">
-    <textarea name="description" cols="30" rows="10"></textarea>
-    @csrf
-    <button id="create-category">Guardar</button>
-</form>
+<div class="mt-5">
+    <div class="card w-50 m-auto">
+        <div class="card-header">
+            <h3>Crear Categoria</h3>
+        </div>
+
+        <div class="card-header">
+            <form action="{{ route("categories.save") }}" method="post">
+                @csrf
+
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input type="text" name="name" class="form-control">
+                    @if ($errors->has('name'))
+                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Descripcion</label>
+                    <textarea name="description" cols="30" rows="5" class="form-control"></textarea>
+                    @if ($errors->has('description'))
+                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                    @endif
+                </div>
+
+                <div class="d-flex align-items-center justify-content-end">
+                    <a href="{{ route("categories.index") }}" class="btn btn-warning text-white mx-1">Cancelar</a>
+                    <button id="create-category" class="btn btn-primary mx-1">Guardar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
